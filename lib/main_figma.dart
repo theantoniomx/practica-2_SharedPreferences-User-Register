4ff71cfd3e3d94b790de495757527c73867fca88
theantoniomx <theantonio.45mx@outlook.com>
2025-05-23 04:54:13 -0600

    Complete HomeScreen and add bottom navigation bar

--- lib/main_figma.dart
+++ lib/main_figma.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:pmsn2025/app_styles.dart';
 import 'package:pmsn2025/size_config.dart';
 
@@ -19,7 +20,8 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: HomeScreen(),
+        backgroundColor: Colors.white,
+        body: const HomeScreen(),
       ),
     );
   }
@@ -40,22 +42,35 @@ class _HomeScreenState extends State<HomeScreen> {
     'tshirt_icon',
     'jeans_icon'
   ];
-  List<String> images = [
-    'image-01',
-    'image-02',
-    'image-03',
-    'image-04',
-    'image-05'
-  ];
+  List<String> images = ['image-01', 'image-02', 'image-03', 'image-04'];
 
   int current = 0;
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
 
-    return SafeArea(
-      child: ListView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Práctica 1 - Figma Challenge',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context); // Regresa a la pantalla anterior
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: kPaddingHorizontal),
@@ -86,66 +101,66 @@ class _HomeScreenState extends State<HomeScreen> {
                 const CircleAvatar(
                   radius: 20,
                   backgroundColor: kGrey,
-                  backgroundImage: NetworkImage(
-                      'https://randomuser.me/api/portraits/men/29.jpg'),
+                  backgroundImage: NetworkImage('https://i.pravatar.cc/300'),
                 )
               ],
             ),
           ),
           const SizedBox(height: 24),
-          Material(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: kPaddingHorizontal,
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    style: kEncodeSansRegular.copyWith(
-                      color: grey5,
-                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
-                    ),
-                    controller: TextEditingController(),
-                    decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 13,
-                        ),
-                        prefixIcon: const IconTheme(
-                          data: IconThemeData(
-                            color: grey6,
+          SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: kPaddingHorizontal,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: kEncodeSansRegular.copyWith(
+                        color: grey5,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                      ),
+                      controller: TextEditingController(),
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 13,
                           ),
-                          child: Icon(Icons.search),
-                        ),
-                        hintText: 'Search clothes. . . ',
-                        border: kInputBorder,
-                        errorBorder: kInputBorder,
-                        focusedBorder: kInputBorder,
-                        disabledBorder: kInputBorder,
-                        focusedErrorBorder: kInputBorder,
-                        enabledBorder: kInputBorder,
-                        hintStyle: kEncodeSansRegular.copyWith(
-                          color: grey6,
-                          fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
-                        )),
+                          prefixIcon: const IconTheme(
+                            data: IconThemeData(
+                              color: grey6,
+                            ),
+                            child: Icon(Icons.search),
+                          ),
+                          hintText: 'Search clothes. . . ',
+                          border: kInputBorder,
+                          errorBorder: kInputBorder,
+                          focusedBorder: kInputBorder,
+                          disabledBorder: kInputBorder,
+                          focusedErrorBorder: kInputBorder,
+                          enabledBorder: kInputBorder,
+                          hintStyle: kEncodeSansRegular.copyWith(
+                            color: grey6,
+                            fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                          )),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                Container(
-                  height: 49,
-                  width: 49,
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: grey13,
-                    borderRadius: BorderRadius.circular(kBorderRadius),
+                  const SizedBox(
+                    width: 16,
                   ),
-                  child: SvgPicture.asset('assets/figma/filter_icon.svg'),
-                )
-              ],
+                  Container(
+                    height: 49,
+                    width: 49,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: grey13,
+                      borderRadius: BorderRadius.circular(kBorderRadius),
+                    ),
+                    child: SvgPicture.asset('assets/figma/filter_icon.svg'),
+                  )
+                ],
+              ),
             ),
-          )),
+          ),
           const SizedBox(
             height: 24,
           ),
@@ -215,23 +230,88 @@ class _HomeScreenState extends State<HomeScreen> {
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 23,
             itemCount: images.length,
             padding: const EdgeInsets.symmetric(
               horizontal: kPaddingHorizontal,
             ),
             itemBuilder: (context, index) {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Stack(
                     children: [
                       Positioned(
-                          child: ClipRRect(
-                        borderRadius: BorderRadius.circular(kBorderRadius),
-                        child: Image.asset(
-                          'assets/figma/images/${images[index]}.png',
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          child: Image.asset(
+                            'assets/figma/images/${images[index]}.png',
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                      ))
+                      ),
+                      Positioned(
+                        right: 12,
+                        top: 12,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: SvgPicture.asset(
+                              'assets/figma/favorite_cloth_icon_unselected.svg'),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Modern Light Clothes',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansSemibold.copyWith(
+                      color: grey10,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                    ),
+                  ),
+                  Text(
+                    'Dress modern',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansRegular.copyWith(
+                      color: grey4,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '\$212.99',
+                        style: kEncodeSansSemibold.copyWith(
+                          color: grey13,
+                          fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.star, color: kYellow, size: 16),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '5.0',
+                            style: kEncodeSansRegular.copyWith(
+                              color: grey13,
+                              fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   )
                 ],
@@ -239,6 +319,51 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        height: 64,
+        child: CustomNavigationBar(
+          isFloating: true,
+          borderRadius: const Radius.circular(40),
+          selectedColor: white1,
+          unSelectedColor: kGrey,
+          backgroundColor: grey13,
+          strokeColor: Colors.transparent,
+          scaleFactor: 0.1,
+          iconSize: 40,
+          items: [
+            CustomNavigationBarItem(
+              icon: _currentIndex == 0
+                  ? SvgPicture.asset('assets/figma/home_icon_selected.svg')
+                  : SvgPicture.asset('assets/figma/home_icon_unselected.svg'),
+            ),
+            CustomNavigationBarItem(
+              icon: _currentIndex == 1
+                  ? SvgPicture.asset('assets/figma/cart_icon_selected.svg')
+                  : SvgPicture.asset('assets/figma/cart_icon_unselected.svg'),
+            ),
+            CustomNavigationBarItem(
+              icon: _currentIndex == 2
+                  ? SvgPicture.asset('assets/figma/favorite_icon_selected.svg')
+                  : SvgPicture.asset(
+                      'assets/figma/favorite_icon_unselected.svg'),
+            ),
+            CustomNavigationBarItem(
+              icon: _currentIndex == 3
+                  ? SvgPicture.asset('assets/figma/account_icon_selected.svg')
+                  : SvgPicture.asset(
+                      'assets/figma/account_icon_unselected.svg'),
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
